ef3bee3ffb681852211b6573647b6a6e597a2467
MichaelFami <35536187+MichaelFami@users.noreply.github.com>
2022-03-04 20:26:51 -0600

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,55 @@
 import React, { useState } from "react";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  function handleFirstChange(event) {
-    setFirstName(event.target.value);
-  }
-  function handleLastChange(event) {
-    setLastName(event.target.value);
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // function handleFirstChange(event) {
+  //   setFirstName(event.target.value);
+  // }
+  // function handleLastChange(event) {
+  //   setLastName(event.target.value);
+  // }
+
+  const [fullName, setFullName] = useState({
+    firstName: "",
+    lastName: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "firstName") {
+        return {
+          firstName: value,
+          lastName: prevValue.lastName
+        };
+      } else if (inputName === "lastName") {
+        return {
+          firstName: prevValue.firstName,
+          lastName: value
+        };
+      }
+    });
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {firstName} {lastName}
+        Hello {fullName.firstName} {fullName.lastName}
       </h1>
       <form>
         <input
-          onChange={handleFirstChange}
-          name="fName"
+          onChange={handleChange}
+          name="firstName"
           placeholder="First Name"
+          value={fullName.firstName}
         />
         <input
-          onChange={handleLastChange}
-          name="lName"
+          onChange={handleChange}
+          name="lastName"
           placeholder="Last Name"
+          value={fullName.lastName}
         />
         <button>Submit</button>
       </form>
